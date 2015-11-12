8d24e61a07d83c883a8e388ba0515959a5464297
Вадим <guts.vadim@gmail.com>
2015-11-12 12:56:56 +0200

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('factoryExample')
+    .controller('PersonListCtrl', function($scope, personsFactory) {
+        $scope.persons = personsFactory.list;
+        $scope.remove = function(id) {
+            personsFactory.remove(id);
+        }
+    })
+    .controller('PersonAddCtrl', function($scope, personsFactory) {
+        $scope.person = {};
+
+        $scope.add = function() {
+            personsFactory.add($scope.person);
+            $scope.person = {};
+        }
+    });
---
+++ js/services.js
@@ -0,0 +1,32 @@
+angular.module('factoryExample')
+    .factory('personsFactory', function() {
+
+        if(!localStorage.getItem('persons'))
+            localStorage.setItem('persons', JSON.stringify([]));
+
+        var persons = JSON.parse(localStorage.getItem('persons'));
+
+        function save() {
+            localStorage.setItem('persons', JSON.stringify(persons));
+        }
+
+        function add(person) {
+            persons.push(person)
+            save();
+        }
+
+        function remove(id) {
+            angular.forEach(persons, function(person, i) {
+                if(person.id == id) {
+                    persons.splice(i, 1);
+                }
+            });
+            save();
+        }
+
+        return {
+            list: persons,
+            add: add,
+            remove: remove
+        }
+    });
